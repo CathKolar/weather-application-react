2fd92f80dd73193d39ca531292c2e4738f1aef55
CathKolar <68386285+CathKolar@users.noreply.github.com>
2020-10-05 18:31:03 +0200

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <small>
-          <a
-            href="https://github.com/CathKolar/weather-application-react"
-            target="blank"
-          >
-            <span className="link"> Open-source code </span>
-          </a>
-          by
-          <a href="https://www.linkedin.com/in/catherinekolar/" target="blank">
-            <span className="link"> Catherine Kolar </span>
-          </a>
-        </small>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <small>
+            <a
+              href="https://github.com/CathKolar/weather-application-react"
+              target="blank"
+            >
+              <span className="link"> Open-source code </span>
+            </a>
+            by
+            <a
+              href="https://www.linkedin.com/in/catherinekolar/"
+              target="blank"
+            >
+              <span className="link"> Catherine Kolar </span>
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
