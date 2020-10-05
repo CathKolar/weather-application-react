84ce985991c9a96e8265138a385668ef4336071c
CathKolar <68386285+CathKolar@users.noreply.github.com>
2020-10-05 13:38:15 +0200

    Add GitHub repo and LinkedIn URLs

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <small>
+          <a
+            href="https://github.com/CathKolar/weather-application-react"
+            target="blank"
+          >
+            <span className="link"> Open-source code </span>
+          </a>
+          by
+          <a href="https://www.linkedin.com/in/catherinekolar/" target="blank">
+            <span className="link"> Catherine Kolar </span>
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
