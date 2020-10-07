815eec6b5f9813ea1d09e55e6932f73b859c7c4d
CathKolar <68386285+CathKolar@users.noreply.github.com>
2020-10-07 18:26:31 +0200

    Add API integration
    Add API integration to Today's weather

--- src/Today.js
+++ src/Today.js
@@ -1,45 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Today.css";
-export default function () {
-  let weatherData = {
-    place: "Seville, ES",
-    description: "Clear sky",
-    imageUrl: "http://openweathermap.org/img/wn/01d@2x.png",
-    high: 41,
-    low: 33,
-    temperature: 35,
-    humidity: 18,
-    wind: 10,
-  };
-  return (
-    <div className="Today">
-      <div className="row current-conditions">
-        <div className="col-4">
-          <img src={weatherData.imageUrl} alt=" " />
-          <h1>
-            <div className="condition-description">
-              {weatherData.description}
-            </div>
-            <span className="temp-high-low-today">
-              <strong>{weatherData.high}</strong>º| {weatherData.low}º
-            </span>
-          </h1>
-        </div>
-        <div className="col-4">
-          <h2>
-            <span>{weatherData.temperature}ºC</span>
-            <span></span>
-            <div>{weatherData.place}</div>
-          </h2>
-        </div>
-        <div className="col-4 weather-extras">
-          <p>
-            Humidity: {weatherData.humidity}%
-            <br />
-            Wind: {weatherData.wind} km/h
-          </p>
+import axios from "axios";
+
+export default function Today(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response);
+    setWeatherData({
+      ready: true,
+      iconUrl: "http://openweathermap.org/img/wn/01d@2x.png",
+      high: response.data.main.temp_max,
+      low: response.data.main.temp_min,
+      feelsLike:response.data.main.feels_like,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      city: props.defaultCity,
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="Today">
+        <div className="row current-conditions">
+          <div className="col-4">
+            <img src={weatherData.iconUrl} alt="weatherData.description" />
+            <h1>
+              <div className="condition-description text-capitalize">
+                {weatherData.description}
+              </div>
+              <span className="temp-high-low-today">
+                <strong>{Math.round(weatherData.high)}</strong>º| {Math.round(weatherData.low)}º
+              </span>
+            </h1>
+          </div>
+          <div className="col-4">
+            <h2>
+              <span>{Math.round(weatherData.temperature)}ºC</span>
+              <span></span>
+              <div>{weatherData.city}</div>
+            </h2>
+          </div>
+          <div className="col-4 weather-extras">
+            <p>
+              Feels like: {Math.round(weatherData.feelsLike)}ºC
+              <br />
+              Humidity: {weatherData.humidity}%
+              <br />
+              Wind: {Math.round(weatherData.wind)} km/h
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "6feaf6a8d604af91166c8484867322e7";
+    let city = props.defaultCity;
+    let unit = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -22,7 +22,7 @@ export default function WeatherApp() {
                 <Search />
               </div>
             </div>
-            <Today />
+            <Today defaultCity="Seville" />
 
             <Forcast />
 
