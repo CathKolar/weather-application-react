c301b5a24faf7463ee719bee2d28f01f7332d961
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-03-02 19:26:43 +0100

    fix typo in Forcast.css, make daily forecast responive

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -1,11 +1,10 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
-
 export default function DailyForecast(props) {
   function day() {
-     let now = new Date(props.data.dt*1000)
-     let days = [
+    let now = new Date(props.data.dt * 1000);
+    let days = [
       "Sunday",
       "Monday",
       "Tuesday",
@@ -15,20 +14,24 @@ export default function DailyForecast(props) {
       "Saturday",
     ];
     let day = days[now.getDay()];
- 
-    return `${day}` ;
- } 
 
-    function temperature() {
+    return `${day}`;
+  }
+
+  function temperature() {
     let temperature = Math.round(props.data.temp.day);
-    return (<div><strong>{temperature}</strong>°C</div>);
-}
+    return (
+      <div>
+        <strong>{temperature}</strong>°C
+      </div>
+    );
+  }
 
-    return(
-            <div className = "dailyForecast col">
-                <div className="day">{day()}</div> 
-                 <WeatherIcon code = {props.data.weather[0].icon} />
-                 <div className="forecast-temperature">{temperature()}</div>
-                  
-          </div>);
+  return (
+    <div className="dailyForecast col-sm">
+      <div className="day">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} />
+      <div className="forecast-temperature">{temperature()}</div>
+    </div>
+  );
 }
