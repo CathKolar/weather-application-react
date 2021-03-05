13172cdb977ce6218ff2ddbb8e2f19c0a6535193
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-03-05 23:14:03 +0100

    improve responsiveness

--- src/Weather.js
+++ src/Weather.js
@@ -26,78 +26,80 @@ export default function Weather(props) {
       longitude: response.data.coord.lon,
     });
   }
- 
 
-   function search (){ 
+  function search() {
     const apiKey = "6feaf6a8d604af91166c8484867322e7";
     let unit = "metric";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
-    axios.get(apiUrl).then(handleResponse);}
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-  function handleSubmit(event){
-       event.preventDefault();
+  function handleSubmit(event) {
+    event.preventDefault();
     search(city);
-   }
-  function handleCityChange(event){setCity(event.target.value);}
-    function currentLocation(position) {
+  }
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+  function currentLocation(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
     let unit = "metric";
     let apiKey = "6feaf6a8d604af91166c8484867322e7";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
-    axios.get(apiUrl).then(handleResponse);}
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   function getCurrentLocation() {
     navigator.geolocation.getCurrentPosition(currentLocation);
   }
 
-  
-   
-
- 
-    
   if (weatherData.ready) {
     return (
       <div className="Weather">
-         <div className="weather-app-wrapper">
-        <div className="card">
-          <div className="card-body">
-            <div className="search-and-locate">
-          <div className="search">
-      <form id="search-city" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Search City"
-          autoFocus="on"
-          autoComplete="off"
-          className="city-input"
-          id="city-input"
-          onChange={handleCityChange}
-        />
-        <input
-          type="submit"
-          value="🔎"
-          className="search-button"
-          id="search-button"
-        />
-      </form>
-       <div className="Locate">
-      <button className="current-location-button" onClick={getCurrentLocation}> ➢ Current Location </button>
-    </div>
-     </div>
-        <WeatherInfo data={weatherData}/>
-      </div>
-
-            <Forcast lat = {weatherData.latitude} lon = {weatherData.longitude}/>
-
-         
+        <div className="weather-app-wrapper">
+          <div className="card">
+            <div className="card-body">
+              <div className="search-and-locate">
+                <div className="search">
+                  <form id="search-city" onSubmit={handleSubmit}>
+                    <input
+                      type="text"
+                      placeholder="Search City"
+                      autoFocus="on"
+                      autoComplete="off"
+                      className="city-input"
+                      id="city-input"
+                      onChange={handleCityChange}
+                    />
+                    <input
+                      type="submit"
+                      value="🔎"
+                      className="search-button"
+                      id="search-button"
+                    />
+                  </form>
+                  <div className="Locate">
+                    <button
+                      className="current-location-button"
+                      onClick={getCurrentLocation}
+                    >
+                      {" "}
+                      ➢ Current Location{" "}
+                    </button>
+                  </div>
+                </div>
+                <WeatherInfo data={weatherData} />
               </div>
-      </div>
-      </div>
+
+              <Forcast lat={weatherData.latitude} lon={weatherData.longitude} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   } else {
-   search()
+    search();
     return <Loader type="TailSpin" color="#f2f2f1" height={100} width={100} />;
   }
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,39 +3,41 @@ import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 import "./WeatherInfo.css";
-export default function WeatherInfo (props){
-    return (
+export default function WeatherInfo(props) {
+  return (
     <div className="WeatherInfo">
-      
-    <div className="row current-conditions">
-          <div className="col-sm-4">
-            <WeatherIcon code={props.data.icon} />
-            <h1>
-              <div className="condition-description text-capitalize">
-                {props.data.description}
-              </div>
-              <span className="temp-high-low-today">
-                <strong>{Math.round(props.data.high)}</strong>º|{" "}
-                {Math.round(props.data.low)}º
-              </span>
-            </h1>
-          </div>
-          <div className="col-sm-4">
-            <h2>
-          <WeatherTemperature celsius={props.data.temperature} />
-            
-              <div className="city">{props.data.city}</div>
-            </h2>
-            <FormattedDate date={props.data.date} />
-          </div>
-          <div className="col-sm-4 weather-extras">
-            <p>
-              Feels like: {Math.round(props.data.feelsLike)}ºC
-              <br />
-              Humidity: {props.data.humidity}%
-              <br />
-              Wind: {Math.round(props.data.wind)} km/h
-            </p>
-          </div>
+      <div className="row row-cols-3 current-conditions">
+        <div className="col-sm-4">
+          <WeatherIcon code={props.data.icon} />
+          <h1>
+            <div className="condition-description text-capitalize">
+              {props.data.description}
+            </div>
+            <span className="temp-high-low-today">
+              <strong>{Math.round(props.data.high)}</strong>º|{" "}
+              {Math.round(props.data.low)}º
+            </span>
+          </h1>
         </div>
-        </div>);}
+        <div className="col-sm-4">
+          <h2>
+            <WeatherTemperature celsius={props.data.temperature} />
+
+            <div className="city">{props.data.city}</div>
+          </h2>
+          <FormattedDate date={props.data.date} />
+        </div>
+
+        <div className="col-sm-4 weather-extras">
+          <p>
+            Feels like: {Math.round(props.data.feelsLike)}ºC
+            <br />
+            Humidity: {props.data.humidity}%
+            <br />
+            Wind: {Math.round(props.data.wind)} km/h
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
